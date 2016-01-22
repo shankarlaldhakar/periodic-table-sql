6945ebc64f15ad5fa1d5ff955e57cc1ca4fac785
eugene <eugene.lifescale@gmail.com>
2016-01-22 20:23:04 +0200

    remove isotope names

--- isotopes/structure.sql
+++ isotopes/structure.sql
@@ -5,7 +5,6 @@ CREATE TABLE `isotope` (
   `neutron` int(11) NOT NULL,
   `isotopic_mass` decimal(10,8) NOT NULL,
   `half_life_seconds` double DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
   `symbol` varchar(45) NOT NULL,
   PRIMARY KEY (`isotope_id`),
   KEY `fk_isotope_element_id_idx` (`element_id`),
