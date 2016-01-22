ad32266f763fa096d38bd43ede487060d7e86755
eugene <eugene.lifescale@gmail.com>
2016-01-22 20:24:58 +0200

    rename fields

--- isotopes/structure.sql
+++ isotopes/structure.sql
@@ -1,11 +1,13 @@
 CREATE TABLE `isotope` (
+
   `isotope_id` int(11) NOT NULL AUTO_INCREMENT,
   `element_id` int(11) NOT NULL,
-  `proton` int(11) NOT NULL,
-  `neutron` int(11) NOT NULL,
-  `isotopic_mass` decimal(10,8) NOT NULL,
-  `half_life_seconds` double DEFAULT NULL,
-  `symbol` varchar(45) NOT NULL,
+  `proton`     int(11) NOT NULL,
+  `neutron`    int(11) NOT NULL,
+  `mass`       decimal(10,8) NOT NULL,
+  `half_life`  double DEFAULT NULL,
+  `symbol`     varchar(45) NOT NULL,
+
   PRIMARY KEY (`isotope_id`),
   KEY `fk_isotope_element_id_idx` (`element_id`),
   CONSTRAINT `fk_isotope_element_id` FOREIGN KEY (`element_id`) REFERENCES `element` (`element_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
