958690b3364559efa00ada29f951bb2ee6ef4014
eugene <eugene.lifescale@gmail.com>
2016-01-22 20:26:57 +0200

    rename fields

--- elements/structure.sql
+++ elements/structure.sql
@@ -1,10 +1,12 @@
 CREATE TABLE `element` (
+
   `element_id` int(11) NOT NULL AUTO_INCREMENT,
-  `group` tinyint(1) DEFAULT NULL,
-  `period` tinyint(1) DEFAULT NULL,
-  `atomic_number` tinyint(3) NOT NULL,
-  `atomic_mass` decimal(10,8) NOT NULL,
-  `symbol` varchar(2) NOT NULL,
-  `name` varchar(45) NOT NULL,
+  `group`      tinyint(1) DEFAULT NULL,
+  `period`     tinyint(1) DEFAULT NULL,
+  `number`     tinyint(3) NOT NULL,
+  `mass`       decimal(10,8) NOT NULL,
+  `symbol`     varchar(2) NOT NULL,
+  `name`       varchar(45) NOT NULL,
+
   PRIMARY KEY (`element_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
