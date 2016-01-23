cfe58d2823a934384ace3368f15f773380e2bbd5
eugene <eugene.lifescale@gmail.com>
2016-01-23 18:12:11 +0200

    add decay chains

---
+++ decay/structure.sql
@@ -0,0 +1,22 @@
+CREATE TABLE `decay` (
+
+  `decay_id`   int(11) NOT NULL AUTO_INCREMENT,
+  `isotope_id` int(11) NOT NULL,
+
+  PRIMARY KEY (`decay_id`),
+  KEY `fk_decay_isotope_id_idx` (`isotope_id`),
+  CONSTRAINT `fk_decay_isotope_id` FOREIGN KEY (`isotope_id`) REFERENCES `isotope` (`isotope_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `decay_mode` (
+
+  `decay_mode_id` int(11) NOT NULL AUTO_INCREMENT,
+  `decay_id`      int(11) NOT NULL,
+  `proton`        int(11) NOT NULL,
+  `neutron`       int(11) NOT NULL,
+  `percent`       int(11) NULL,
+
+  PRIMARY KEY (`decay_mode_id`),
+  KEY `fk_decay_mode_decay_id_idx` (`decay_id`),
+  CONSTRAINT `fk_decay_mode_decay_id` FOREIGN KEY (`decay_id`) REFERENCES `decay` (`decay_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
